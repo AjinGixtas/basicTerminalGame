72119849d2cce471b4d63e82f4e1e8fedb1359c7
An :) <tdinhan999@gmail.com>
2025-04-28 16:50:27 +0700

    Minor change
    Remove removed variables.

--- Scripts/CS/NetworkManager.cs
+++ Scripts/CS/NetworkManager.cs
@@ -41,7 +41,6 @@ public partial class NetworkManager : Node
                 parentNode.ChildNode.Add(node);
                 ++poolIndex;
             }
-            lastLayer = curLayer; curLayer = [];
         }
         return network;
     }
